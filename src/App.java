a922fe8e2d90462fd260620630dd55ce992bf1f4
sicalo330 <noah.esteban9@gmail.com>
2024-04-09 00:22:33 -0500

    Singletonizar statement con wirkbench

--- src/App.java
+++ src/App.java
@@ -1,25 +1,34 @@
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class App {
     // Conexion con workbench mysql, es necesario tener xampp instalado y tener el
     // puerto 3306 corriendo para que esto funcione,
     public static void main(String[] args) throws Exception {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            // El nombre de la base de datos debe cambiar |
-            // v
-            // Lo otro es el nombre del usuario y la contraseña (root y contraseña vacía en
-            // este caso)
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebaSingleton", "root",
-                    "");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select nombre from data");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(1));
+            Singleton singleton = Singleton.getInstance();
+            Singleton singleton2 = Singleton.getInstance();
+            try (Statement statement = singleton.getStatement()) {
+                System.out.println(singleton);
+                System.out.println(singleton2);
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                // El nombre de la base de datos debe cambiar
+                // Lo otro es el nombre del usuario y la contraseña (root y contraseña vacía en
+                // este caso)
+                ResultSet resultSet = statement.executeQuery("select nombre, apellido from data");
+                while (resultSet.next()) {
+                    System.out.println(
+                            "Primer nombre " + resultSet.getString(1) + " Segundo nombre " + resultSet.getString(2));
+                }
+                statement.getConnection().close();
+            } catch (Exception e) {
+                System.out.println("Error en la conexion " + e);
             }
-            connection.close();
-        } catch (Exception e) {
-            System.out.println("Error en la conexion " + e);
+        } catch (SQLException e) {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            e.printStackTrace(System.err);
         }
     }
 }
---
+++ src/Singleton.java
@@ -0,0 +1,46 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public final class Singleton {
+    // La instancia única de la clase
+    private static Singleton instancia;
+
+    private Connection connection;
+    private Statement statement;
+
+    // Constructor privado para evitar la creación de instancias desde fuera de la
+    // clase
+    private Singleton() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebaSingleton", "root", "");
+        statement = connection.createStatement();
+    }
+
+    // Método estático para obtener la instancia única de la clase
+    public static Singleton getInstance() throws SQLException {
+        // Si la instancia aún no ha sido creada, la creamos
+        if (instancia == null) {
+            instancia = new Singleton();
+        }
+        // Retornamos la instancia única
+        return instancia;
+    }
+
+    public Statement getStatement() {
+        return statement;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void close() throws SQLException {
+        if (statement != null) {
+            statement.close();
+        }
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
